5263549526774dfc49737eb16370e33488d3d220
Steven Mahana <stevenmahana@gmail.com>
2016-04-25 07:53:58 -0700

    rebuild shell script

--- app/rebuild_messaging.sh
+++ app/rebuild_messaging.sh
@@ -1,24 +1,22 @@
-#!/usr/bin/env bash
-# ~/.bash_aliases
+#!/bin/bash
 
-# Kill all running containers.
-#alias dockerkillall='docker kill $(docker ps -q)'
-
-# Kill all running containers.
-# alias dockerkillall='docker-compose stop'
-
-# Delete all stopped containers.
-#alias dockercleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
-
-# Delete all untagged images.
-#alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
-
-# Delete all stopped containers and untagged images.
-#alias dockerclean='dockercleanc || true && dockercleani'
-
+# stop containers
 docker-compose stop
+
+# Remove all stopped containers:
 docker rm $(docker ps -a | grep Exited | awk '{print $1}')
+
+# Clean up un-tagged docker images:
 docker rmi $(docker images -q --filter "dangling=true")
+
+# Stop and remove all containers (including running containers!)
 docker rm -f $(docker ps -a -q)
+
+# Remove messging image
 docker rmi -f app_messaging
+
+# Run cleanup
+bash cleanup.sh
+
+# rebuild containers
 docker-compose up
